215853e342097b6806a5b0bdfc2cca5c3cfea83d
Jeffery Utter <jeff@jeffutter.com>
2013-12-06 09:27:39 -0600

    Print name of repo being deployed

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.router.path('/', function () {
   this.post(function () {
     repo = this.req.body.repository.url.match(/\:(.*)\.git/)[1];
     name = this.req.body.repository.name;
+    console.log('Deploying: '+name);
     exec('bash repos/'+name+'.sh',function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
